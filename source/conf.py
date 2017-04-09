41d05fa1201e450d785f5ecbd2d2b6be17d284a9
ron <ron.trompert@surfsara.nl>
2017-04-09 23:32:07 +0200

    changed Grid for SWIFT

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Grid Documentation documentation build configuration file, created by
+# SWIFT Documentation documentation build configuration file, created by
 # sphinx-quickstart on Tue Dec 16 11:32:42 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Grid Documentation'
-copyright = u'2015, Grid Support <helpdesk@surfsara.nl>'
+project = u'SWIFT Documentation'
+copyright = u'2015, SWIFT Support <helpdesk@surfsara.nl>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -176,7 +176,7 @@ html_title = "{} v{}".format(project, release)
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'GridDocumentationdoc'
+htmlhelp_basename = 'SwiftDocumentationdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'GridDocumentation.tex', u'Grid Documentation Documentation',
-   u'Grid Support \\textless{}helpdesk@surfsara.nl\\textgreater{}', 'manual'),
+  ('index', 'SwiftDocumentation.tex', u'SWIFT Documentation Documentation',
+   u'SWIFT Support \\textless{}helpdesk@surfsara.nl\\textgreater{}', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'griddocumentation', u'Grid Documentation Documentation',
-     [u'Grid Support <helpdesk@surfsara.nl>'], 1)
+    ('index', 'swiftdocumentation', u'SWIFT Documentation Documentation',
+     [u'SWIFT Support <helpdesk@surfsara.nl>'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'GridDocumentation', u'Grid Documentation Documentation',
-   u'Grid Support <helpdesk@surfsara.nl>', 'GridDocumentation', 'One line description of project.',
+  ('index', 'SwiftDocumentation', u'SWIFT Documentation Documentation',
+   u'SWIFT Support <helpdesk@surfsara.nl>', 'SwiftDocumentation', 'One line description of project.',
    'Miscellaneous'),
 ]
 

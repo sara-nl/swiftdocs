99f96019dbf9b887101ee1314addf068c92e733d
Ron Trompert <ron.trompert@surf.nl>
2023-11-04 21:55:55 +0100

    a

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# SWIFT Documentation documentation build configuration file, created by
+# SURF Object Store Documentation documentation build configuration file, created by
 # sphinx-quickstart on Tue Dec 16 11:32:42 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'SWIFT Documentation'
-copyright = u'2015, SWIFT Support <https://servicedesk.surf.nl/>'
+project = u'Object Store Documentation'
+copyright = u'2015, SURF Support <https://servicedesk.surf.nl/>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'SwiftDocumentation.tex', u'SWIFT Documentation Documentation',
-   u'SWIFT Support \\textless{}https://servicedesk.surf.nl/\\textgreater{}', 'manual'),
+  ('index', 'SwiftDocumentation.tex', u'Object Store Documentation Documentation',
+   u'SURF Support \\textless{}https://servicedesk.surf.nl/\\textgreater{}', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,7 +226,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'swiftdocumentation', u'SWIFT Documentation Documentation',
+    ('index', 'swiftdocumentation', u'Object Store Documentation Documentation',
      [u'SWIFT Support <https://servicedesk.surf.nl/>'], 1)
 ]
 
@@ -240,7 +240,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'SwiftDocumentation', u'SWIFT Documentation Documentation',
+  ('index', 'SwiftDocumentation', u'Object Store Documentation Documentation',
    u'SWIFT Support <https://servicedesk.surf.nl/>', 'SwiftDocumentation', 'One line description of project.',
    'Miscellaneous'),
 ]

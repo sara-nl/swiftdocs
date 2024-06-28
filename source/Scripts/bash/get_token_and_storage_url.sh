15503d55c78a1ef675772791660cde98bcb117b4
Ron Trompert <ron.trompert@surf.nl>
2024-06-28 22:40:09 +0200

    ported to python3

--- source/Scripts/bash/get_token_and_storage_url.sh
+++ source/Scripts/bash/get_token_and_storage_url.sh
@@ -43,24 +43,23 @@ PYTHONSCRIPT=`mktemp`
 chmod 755 ${PYTHONSCRIPT}
 
 cat > ${PYTHONSCRIPT} << EOF
-#!/usr/bin/env python
+#!/usr/bin/python3
 import sys, json, re
 list=json.load(sys.stdin)["token"]["catalog"]
 for i in list:
     if i["type"]=="object-store" and re.search('swift',i["name"])!=None:
         for j in i["endpoints"]:
             if j["interface"]=="public":
-                print "export OS_STORAGE_URL="+j["url"]
+                print ("export OS_STORAGE_URL="+j["url"])
 EOF
 
 curl -si  \
   -H "Content-Type: application/json" \
   -o ${TMPFILE} \
   -d @${JSONFILE} \
- ${OS_AUTH_URL}/auth/tokens 2>/dev/null | grep 'X-Subject-Token:' | awk '{print $2}'
-
+ ${OS_AUTH_URL}/auth/tokens 2>/dev/null | grep -i 'X-Subject-Token:' | awk '{print $2}'
 echo
-token=`cat ${TMPFILE} | grep 'X-Subject-Token:' | awk '{print $2}'`
+token=`cat ${TMPFILE} | grep -i 'X-Subject-Token:' | awk '{print $2}'`
 echo "export OS_AUTH_TOKEN="${token}
 
 echo
